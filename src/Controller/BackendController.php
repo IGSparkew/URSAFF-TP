75679de9b3c4de13ef7f893b56bda1262cd781d4
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2023-11-06 17:51:31 +0100

    add some routes GET and POST

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -5,19 +5,40 @@ namespace App\Controller;
 use App\Service\CompanyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/api-ouverte-ent-liste')]
 class BackendController extends AbstractController
 {
     private const BASE_URL_API= '/api-ouverte-ent-liste';
     
     public function __construct(private CompanyService $companyService) { }
 
-    #[Route(self::BASE_URL_API .'/', name: 'app_backend')]
+    #[Route('/', name: 'get_all', methods:['GET'] )]
     public function index():JsonResponse
     {   
         $result = $this->companyService->getAllCompany();
         return $this->json($result, 200);
+    }
+
+    #[Route('/search', name: 'search', methods:['GET'])]
+    public function search(Request $request):JsonResponse
+    {   
+        $siren = $request->get('siren');
+        $result = $this->companyService->getAllCompany($siren);
+        if (empty($result)) {
+            return new JsonResponse("Error no companies found!",404);
+        }
+
+
+        return $this->json($result, 200);
+    }
+
+    #[Route('/', name: 'create', methods:['POST'] )]
+    public function create():JsonResponse
+    {   
+        return $this->json("test");
     }
 }
--- src/Service/CompanyService.php
+++ src/Service/CompanyService.php
@@ -4,9 +4,9 @@ namespace App\Service;
 
 class CompanyService {
 
-    public function getAllCompany(int $id=null): array {
+    public function getAllCompany(string $siren=null): array {
     
-        if ($id == null) {
+        if ($siren == null) {
             // if id null return all  
         }
 
