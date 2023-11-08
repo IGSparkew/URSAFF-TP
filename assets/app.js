2ea00c71a21c24dce3ca9a19f4c024f20506f8c1
MartinetC <71493398+MartinetCl@users.noreply.github.com>
2023-11-08 10:44:56 +0100

    ajout partie front

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,172 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+const $ = require("jquery");
+
+let _data = []
+$(document).ready(function () {
+    $(document).on('click', '#search', function (e) {
+        e.preventDefault()
+        $.get('https://recherche-entreprises.api.gouv.fr/search?q=' + $('input[name=recherche]').val(), function () { })
+            .done(function (e) {
+                if (e.results.length > 0) {
+                    _data = e.results
+                    $('.searchList').html('')
+                    $.each(e.results, function (key, data) {
+                        $('.searchList').append(`<tr>
+                        <td>${data.nom_complet}</td>
+                        <td>${data.siren}</td>
+                        <td>${data.siege.siret}</td>
+                        <td>${data.siege.adresse}</td>
+                        <td><button class="choose" data-key="${key}">Choisir</button></td>
+                    </tr>`)
+                    })
+                }
+            })
+    })
+
+
+    $(document).on('click', '.choose', function (e) {
+        e.preventDefault()
+        let data = { 'jsonData': _data[$(this).attr('data-key')] }
+        console.log($(this))
+        $.post('/save', data, function () { })
+            .done(function (e) {
+                if (e.status) {
+                    $('#text').html(`L'élement choisi est : ${data.jsonData.nom_complet}, <br> avec un SIREN : ${data.jsonData.siren}, <br> et un SIRET : ${data.jsonData.siege.siret}, <br> localisé : ${data.jsonData.siege.adresse} <br><br>
+                    <input type="number" name="salaireBrut">
+                    <button class="calculate">Calculer</button>`)
+                }
+            })
+    })
+
+    $(document).on('click', '.calculate', function (e) {
+        e.preventDefault()
+        let value = $('input[name=salaireBrut]').val()
+        $('.bodyContent').html('')
+        salaireNetCdi(value)
+        salaireNetAlternant(value)
+        salaireNetCdd(value)
+        gratificationMinStage()
+    })
+
+    function salaireNetCdi(salaire) {
+        let data = {
+            "situation": {
+                "salarié . contrat . salaire brut": {
+                    "valeur": salaire,
+                    "unité": "€ / mois"
+                },
+                "salarié . contrat": "'CDI'"
+            },
+            "expressions": [
+                "salarié . rémunération . net . à payer avant impôt",
+                "salarié . coût total employeur",
+                "salarié . cotisations"
+            ]
+        }
+
+        $.ajax({
+            url: 'https://mon-entreprise.urssaf.fr/api/v1/evaluate',
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (e) {
+
+                $('.bodyContent').append(`<h3>Salaire net CDI</h3>
+                <p>Rémunération net ${e.evaluate[0].nodeValue}<br>
+                Coût employeur : ${e.evaluate[1].nodeValue}<br>
+                Cotisation : ${e.evaluate[2].nodeValue}</p>`)
+            }
+        })
+    }
+
+    function gratificationMinStage() {
+        let data = {
+            "situation": {
+                "salarié . contrat": "'Stage'"
+            },
+            "expressions": [
+                "salarié . contrat . stage . gratification minimale"
+            ]
+        }
+
+        $.ajax({
+            url: 'https://mon-entreprise.urssaf.fr/api/v1/evaluate',
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (e) {
+                $('.bodyContent').append(`<h3>Gratification min.</h3>
+                <p>Gratification min. : ${e.evaluate[0].nodeValue}</p>`)
+            }
+        })
+    }
+
+    function salaireNetAlternant(salaire) {
+        let data = {
+            "situation": {
+                "salarié . contrat . salaire brut": {
+                    "valeur": salaire,
+                    "unité": "€ / mois"
+                },
+                "salarié . contrat": "'Alternance'"
+            },
+            "expressions": [
+                "salarié . rémunération . net . à payer avant impôt",
+                "salarié . coût total employeur",
+                "salarié . cotisations",
+            ]
+        }
+
+        $.ajax({
+            url: 'https://mon-entreprise.urssaf.fr/api/v1/evaluate',
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (e) {
+                $('.bodyContent').append(`<h3>Salaire net alternant</h3>
+                <p>Rémunération net : ${e.evaluate[0].nodeValue}<br>
+                Coût employeur : ${e.evaluate[1].nodeValue}<br>
+                Cotisations : ${e.evaluate[2].nodeValue}</p>`)
+            }
+        })
+    }
+
+    function salaireNetCdd(salaire) {
+        let data = {
+            "situation": {
+                "salarié . contrat . salaire brut": {
+                    "valeur": salaire,
+                    "unité": "€ / mois"
+                },
+                "salarié . contrat": "'CDD'"
+            },
+            "expressions": [
+                "salarié . rémunération . net . à payer avant impôt",
+                "salarié . coût total employeur",
+                "salarié . cotisations",
+                "salarié . rémunération . indemnités CDD"
+            ]
+        }
+
+        $.ajax({
+            url: 'https://mon-entreprise.urssaf.fr/api/v1/evaluate',
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (e) {
+                $('.bodyContent').append(`<h3>Salaire net CDD</h3>
+                <p>Rémunération net : ${e.evaluate[0].nodeValue}<br>
+                Coût employeur : ${e.evaluate[1].nodeValue}<br>
+                Cotisations : ${e.evaluate[2].nodeValue}<br>
+                Indémnitées : ${e.evaluate[3].nodeValue}</p>`)
+            }
+        })
+    }
+})
--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,14 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
     #[Route('/', name: 'app_front')]
-    public function index() : Response
+    public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('base.html.twig');
+    }
+
+    #[Route('/save', name: 'save')]
+    public function save(Request $request): JsonResponse
+    {
+        $data = $request->get('jsonData');
+        $file = [];
+        if (is_file('savedCompany.json')) {
+            $file = json_decode(file_get_contents('savedCompany.json'), true);
+        }
+        $file[$data['siren']] = $data;
+        file_put_contents('savedCompany.json', json_encode($file));
+        $_SESSION['savedData'] = $data;
+        return $this->json(['message' => 'OK', 'status' => 1]);
     }
 }
